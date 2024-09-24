c1eb86de045f87c78a12ab14474c03a782be6be7
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-09-24 20:15:33 +0530

    frontend completed

---
+++ src/constants.ts
@@ -0,0 +1,26 @@
+const SWATCHES = [
+    "#ffffff", // White
+    "#ff0000", // Red
+    "#ff4500", // Orange Red
+    "#ffa500", // Orange
+    "#ffff00", // Yellow
+    "#32cd32", // Lime Green
+    "#008000", // Green
+    "#00ff7f", // Spring Green
+    "#00ffff", // Cyan
+    "#1e90ff", // Dodger Blue
+    "#0000ff", // Blue
+    "#8a2be2", // Blue Violet
+    "#ff69b4", // Hot Pink
+    "#ff1493", // Deep Pink
+    "#ffc0cb", // Pink
+    "#a52a2a", // Brown
+    "#8b4513", // Saddle Brown
+    "#808080", // Gray
+    "#d3d3d3", // Light Gray
+    "#800000", // Maroon
+    "#ffdab9", // Peach Puff
+    "#deb887", // Burly Wood
+];
+
+export { SWATCHES };
